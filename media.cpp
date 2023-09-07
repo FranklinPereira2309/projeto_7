57628898a1131e2c76b76841dfd23a7b0f455e5d
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-09-07 12:11:43 -0300

    exercico finalizado com o resultado esperado

--- media.cpp
+++ media.cpp
@@ -4,39 +4,56 @@
 using namespace std;
 
 int main() {
-    int i, sair = 1, matricula;
-    float media, soma, nota[4] = {};
 
+    // #define COL  4
+    // #define LIN  10
+
+    int i = 0, j, sair = 1, matricula[10], conte = 0;
+    float media, soma, nota[10][4] = {};
+    
 
     while(sair == 1) {
-    cout << "Digite a matrícula do aluno (8 digitos): ";
-    cin >> matricula;
-    soma = 0;
-    media = 0;
-
-    for(i = 0 ; i < 3 ; i++) {
-        cout << "Digite a nota " << i +1 << " ";
-        cin >> nota[i];
-        soma += nota[i];
-    }
-    media = soma / i;
-    nota[3] = media;
-   
-    system("cls");
-
-    cout << "Matricula\t" << "Nota1\t" << "Nota2\t" << "Nota3\t" << "Media" << endl;
-    cout << "==============================================" << endl;
-    cout << matricula << "\t";
-    for(i = 0 ; i < 4 ; i++) {
-        cout << fixed << setprecision(2) << nota[i] << "\t";
-    }
-
-    cout << endl;
+        soma = 0;
+        media = 0;
+        cout << "Digite a matrícula do aluno (8 digitos): ";
+        cin >> matricula[i];
         
-    cout << "Deseja continuar 1 - Sair 0? ";
-    cin >> sair;
+        for(j = 0 ; j < 3 ; j++) {
+            cout << "Digite a nota " << i +1 << " de " << j +1 << ": ";
+            cin >> nota[i][j];
+            soma += nota[i][j];
 
+        }
+        media = soma / j;
+        nota[i][3] = media;
+        ++i;
+        cout << "Deseja continuar 1 - Sair 0? ";               
+        cin >> sair;
+
+        if(sair == 0) {
+            conte = i;
+        }
     }
+   
+    
+    
+
+    for(i = 0 ; i < conte ; i++) {
+        cout << endl;
+        cout << "Matricula\t" << "Nota1\t" << "Nota2\t" << "Nota3\t" << "Media" << endl;
+        cout << "==============================================" << endl;
+        cout << matricula[i] << "\t";
+        for(j = 0 ; j < 4 ; j++) {
+            cout << fixed << setprecision(2) << nota[i][j] << "\t";
+
+        }
+        cout << endl;
+    }
+
+    
+        
+
+    
 
 
 }
