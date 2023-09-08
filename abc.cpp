d04522e57bd35fe8d5a3c7b78743f78333b6a4d3
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-09-08 19:48:33 -0300

    ajuste nas casas da var indice

--- abc.cpp
+++ abc.cpp
@@ -8,11 +8,14 @@ int main() {
     char vet[31] = {"ABCDEFGHIJLMNOPQRSTUVZWYZ"};
     int numero, indice;
 
-    cout << "Digite um número inteiro: ";
+    cout << "Digite um número inteiro(): ";
     cin >> numero;
 
     for(int i = 0 ; i < numero ; i++) {
        for(int j = 0; j <= i ; j++) {
+            if(indice == 25) {
+                indice = 0;
+            }
             cout << vet[indice] ;
             indice +=1;              
             
